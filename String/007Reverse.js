fbd92c2385e64d5823b69bd14f83ee3866509a2a
f622u <f622u@sina.com>
2019-10-09 21:49:29 +0800

    fix(string):捋清007题的思路

--- String/007Reverse.js
+++ String/007Reverse.js
@@ -7,18 +7,23 @@
 /**
  * 第一种解法：暴力法
  * 解释：主要需要解决的是问题是「溢出」。
- * 执行用时：88ms，内存消耗：35.9MB
+ * 执行用时：92ms，内存消耗：35.9MB
  * 时间复杂度：O(n)。进行一次遍历。
  * 空间复杂度：O(1)。占用一个标记位。
  */
 var reverse = function (x) {
-  var re = 0;
-  while (parseInt(x / 10)) { // 小数点向左移动一位取整后大于0即为true，小于0即为false
-      re = 10 * re + x - 10 * parseInt(x / 10); // 累加器
-      x = parseInt(x / 10); // new x 为 old x 小数点向左移动一位
+  let num = 0;
+  a = Math.abs(x); // 绝对值
+  while (a != 0) {
+    temp = a % 10; // 取余
+    num = num * 10 + temp // 叠加
+    a = parseInt(a / 10) // 除以10并取整
   }
-  if (re > 214748364 || re < -214748364) return 0; // 如果溢出
-  if ((re == 214748364 && x > 7) || (re == 214748364 && x < -8)) return 0;
-  re = 10 * re + x;
-  return re
+  if (x < 0 && num < Math.pow(2, 31)) { // 溢出上限
+    return -num;
+  } else if (x > 0 && num < Math.pow(2, 31) - 1) { // 溢出下限
+    return num;
+  } else {
+    return 0;
+  }
 };
