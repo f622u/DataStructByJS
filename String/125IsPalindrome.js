87a42c14390e6d43df49eadd234feb985c041abc
f622u <f622u@sina.com>
2019-10-15 23:59:44 +0800

    feat(string):增加125题的解答

---
+++ String/125IsPalindrome.js
@@ -0,0 +1,11 @@
+/**
+ * 验证回文串
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+  if(s === '') return true
+  let reg = /[^A-Za-z0-9]/g// ^表示匹配除了中括号外的字符
+  s = s.replace(reg,'').toLowerCase()  
+  return s == s.split('').reverse().join('')
+};
